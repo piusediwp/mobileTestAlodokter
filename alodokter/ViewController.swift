1d9ea5d9fad0fde9d51843d4f20b37e9e1a4fe43
Pius Edi Werda Pebriujianta <piusediwerda@gmail.com>
2019-08-27 17:28:50 +0700

    Initial Commit

---
+++ alodokter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alodokter
+//
+//  Created by Pius Edi Werda Pebriujianta on 27/08/19.
+//  Copyright © 2019 Pius Edi Werda Pebriujianta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
